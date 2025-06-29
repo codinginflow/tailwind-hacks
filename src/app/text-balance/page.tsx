3885c0ef0533b150cd34683b41ed44f0e111f741
Coding in Flow <info@codinginflow.com>
2025-06-29 09:46:55 +0200

    fix: change text-balance headline

--- src/app/text-balance/page.tsx
+++ src/app/text-balance/page.tsx
@@ -2,7 +2,7 @@ export default function Page() {
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-center text-4xl leading-tight font-bold text-balance md:text-6xl lg:text-7xl">
-        Subscribe to Coding in Flow now!
+        Subscribe to Coding in Flow!
       </h1>
     </div>
   );
