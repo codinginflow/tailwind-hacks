37b65ccfd7ba6fb338a65995040a4891d6c4534f
Coding in Flow <info@codinginflow.com>
2025-06-29 09:45:08 +0200

    fix: add text-balance class

--- src/app/text-balance/page.tsx
+++ src/app/text-balance/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="text-center text-4xl leading-tight font-bold md:text-6xl lg:text-7xl">
+      <h1 className="text-center text-4xl leading-tight font-bold text-balance md:text-6xl lg:text-7xl">
         Subscribe to Coding in Flow now!
       </h1>
     </div>
